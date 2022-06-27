6b97a2e573937911efe97dce7d6b3c67c9c4f391
gabriel <gabrielsantanasilva85@gmail.com>
2022-06-27 10:18:49 -0300

    [fix] digit rotation

--- cesar/index.js
+++ cesar/index.js
@@ -1,25 +1,27 @@
 const alph = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const ALPH = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 const [,, text, rotation] = process.argv
 const textAsArray = text.split('')
 let rotationNumber = Number(rotation)
 
+while (rotationNumber >= alph.length) {
+  rotationNumber -= alph.length
+}
+
 const encryptedText = textAsArray.map((character) => {
-  if (alph.includes(character)) {
-    if (rotationNumber > alph.length) {
-      rotationNumber /= alph.length
-    }
+  const source = (alph.includes(character) && alph) || (ALPH.includes(character) && ALPH)
+  if (source) {
+    const letterPosition = source.indexOf(character)
 
-    const letterPosition = alph.indexOf(character)
-
-    if ((letterPosition + rotationNumber) >= alph.length) {
-      const encryptedPosition = (letterPosition + rotationNumber) - alph.length
-      return alph[encryptedPosition]
+    if ((letterPosition + rotationNumber) >= source.length) {
+      const encryptedPosition = (letterPosition + rotationNumber) - source.length
+      return source[encryptedPosition]
     }
 
     const encryptedPosition = letterPosition + rotationNumber
 
-    return alph[encryptedPosition]
+    return source[encryptedPosition]
   }
 
   const isNotANumber = isNaN(character)
@@ -27,14 +29,10 @@ const encryptedText = textAsArray.map((character) => {
   if (!isNotANumber) {
     const characterAsNumber = Number(character)
 
-    if (characterAsNumber + rotationNumber >= 10) {
-      return (characterAsNumber + rotationNumber) - 10
-    }
-
-    return characterAsNumber + rotationNumber
+    return (characterAsNumber + rotationNumber) % 10
   }
 
-  return letter
+  return character
 })
 
 console.log(encryptedText.join(''))
